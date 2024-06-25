b2da78d3d430ef7573996f284c91a9edf3e6b95e
Ethan Ryan <144191914+JungitRyan@users.noreply.github.com>
2024-06-25 12:59:43 -0400

    Style grid for proper formatting

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function makeGrid() {
   for (let i = 0; i < 16; i++) {
-    const col = document.createElement("div");
+    const row = document.createElement("div");
+    row.classList.add("row");
     for (let i = 0; i < 16; i++) {
-      const row = document.createElement("div");
-      row.classList.add("box");
-      row.style.border = "2px solid black";
-      col.appendChild(row);
+      const box = document.createElement("div");
+      box.classList.add("box");
+      row.appendChild(box);
     }
     const container = document.querySelector(".container");
-    container.appendChild(col);
+    container.appendChild(row);
   }
 }
 
+// main method
 makeGrid();
